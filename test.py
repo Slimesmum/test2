674d208d2173526041488961be0136cdce65edc0
Eunice Egeonu <egeonueunice11@gmail.com>
2021-10-10 13:56:50 +0100

    inital commit

---
+++ test.py
@@ -0,0 +1,7 @@
+name = input("Hi what is your name?")
+age = int(input("How old are you?"))
+
+if (age > 18):
+    print("welcome you can register", name)
+else:
+    print("you are too young to register", name)
